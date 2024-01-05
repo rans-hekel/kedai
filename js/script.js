837d7b3dc9e582b75b2a828ae25a6a24a1717f32
rans-hekel <ransenju27@gmail.com>
2024-01-05 23:49:00 +0700

    menambahkan fitur cart

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
-// toggle class active
-let navbarNav = document.querySelector('.navbar-nav');
-// ketika hamburger menu di klik 
-document.querySelector('#hamburger-menu');
+// toggle class active untuk hamburger menu
+let navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu");
 onclick = () => {
-    navbarNav.classList.toggle('active');
-
+  navbarNav.classList.toggle("active");
 };
 
-// klik diluar sidebar untuk menghilangkan navnya
-let hamburger = document.querySelector('#hamburger-menu');
+// toggle untuk class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
+document.querySelector("#search-button").onclick = (e) => {
+  e.preventDefault();
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+};
+
+// toggle class active untuk shopping-cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik diluar element
+let hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector(".shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
